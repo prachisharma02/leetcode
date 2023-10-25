32fe35d59f8c457cc1db288170a07ef62e7b2bd8
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-25 16:18:39 +0530

    Time: 2 ms (100.00%) | Memory: 54.1 MB (81.72%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -6,27 +6,19 @@ class Solution {
         int start = 0;
         int end = k - 1;
         int sum = 0;
-
-        // Calculate the sum of the first k elements
         for (int i = start; i <= end; i++) {
             sum += nums[i];
         }
-
-        double maxavg = (double) sum / k; // Initialize maxavg with the average of the first k elements
-
+        double maxavg = (double) sum / k; 
         while (end < nums.length - 1) {
-            // Update the sum by subtracting the element that is no longer part of the window and adding the new element
             sum = sum - nums[start] + nums[end + 1];
             start++;
             end++;
-
-            double avg = (double) sum / k; // Calculate the average for the current window
-
+            double avg = (double) sum / k; 
             if (avg > maxavg) {
                 maxavg = avg;
             }
         }
-
         return maxavg;
     }
 }
